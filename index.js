03c1b3c500ea2fa81747b6210083d997c7374e8e
Leo <torrest.leonidas@gmail.com>
2023-10-12 13:26:20 +1100

    Solving like buttom bug

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-document
-  .querySelector(".cards__like-button")
-  .addEventListener("click", function () {
-    console.log("hola")
-    this.classList.toggle("filled");
-  });
+function hiddenOutlined() {
+  const likeButtonOutlined = document.querySelector(
+    ".cardslike-button_type_outlined"
+  );
+  const likeButtonFilled = document.querySelector(
+    ".cardslike-button_type_filled"
+  );
+  likeButtonOutlined.style.display = "none";
+  likeButtonFilled.style.display = "inline";
+}
+
+function hiddenFilled() {
+  const likeButtonOutlined = document.querySelector(
+    ".cardslike-button_type_outlined"
+  );
+  const likeButtonFilled = document.querySelector(
+    ".cardslike-button_type_filled"
+  );
+  likeButtonOutlined.style.display = "inline";
+  likeButtonFilled.style.display = "none";
+}
+
+const popupSelector = document.querySelector(".profile__button");
