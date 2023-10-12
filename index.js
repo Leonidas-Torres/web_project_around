995d0348a12a37deeea80a7c17c2d341fbcc197e
Leo <torrest.leonidas@gmail.com>
2023-10-12 13:39:02 +1100

    Like buttom fixed

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-function hiddenOutlined() {
-  const likeButtonOutlined = document.querySelector(
+function hiddenOutlined(event) {
+  const currentCard = event.target.closest(".cards__grid");
+  const likeButtonOutlined = currentCard.querySelector(
     ".cardslike-button_type_outlined"
   );
-  const likeButtonFilled = document.querySelector(
+  const likeButtonFilled = currentCard.querySelector(
     ".cardslike-button_type_filled"
   );
   likeButtonOutlined.style.display = "none";
   likeButtonFilled.style.display = "inline";
 }
 
-function hiddenFilled() {
-  const likeButtonOutlined = document.querySelector(
+function hiddenFilled(event) {
+  const currentCard = event.target.closest(".cards__grid");
+  const likeButtonOutlined = currentCard.querySelector(
     ".cardslike-button_type_outlined"
   );
-  const likeButtonFilled = document.querySelector(
+  const likeButtonFilled = currentCard.querySelector(
     ".cardslike-button_type_filled"
   );
   likeButtonOutlined.style.display = "inline";
